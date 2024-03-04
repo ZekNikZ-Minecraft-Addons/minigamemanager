f95692d8ba762a033ff69b81266245a110c52193
Matthew McCaskill <mattrmccaskill@gmail.com>
2024-03-04 17:44:53 -0600

    resource packs

--- src/main/java/io/zkz/mc/minigamemanager/MinigameManagerPlugin.kt
+++ src/main/java/io/zkz/mc/minigamemanager/MinigameManagerPlugin.kt
@@ -1,5 +1,13 @@
 package io.zkz.mc.minigamemanager
 
 import io.zkz.mc.gametools.GTPlugin
+import io.zkz.mc.gametools.resourcepack.IProvidesResourcePackParts
+import io.zkz.mc.gametools.resourcepack.ResourcePackBuilder
 
-class MinigameManagerPlugin : GTPlugin<MinigameManagerPlugin>()
+class MinigameManagerPlugin : GTPlugin<MinigameManagerPlugin>(), IProvidesResourcePackParts {
+    override fun buildResourcePack(builder: ResourcePackBuilder) {
+        builder.apply {
+            withNegativeSpaceCharacters()
+        }
+    }
+}
